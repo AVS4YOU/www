fa3168e09102906da2954ef40aeab44daf1d665d
Alexey Kostenko <kostenko.20101994@mail.ru>
2019-11-05 14:11:11 +0300

    Add google-fonts plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,15 @@
 module.exports = {
-  plugins: [`gatsby-plugin-styled-components`],
+  plugins: [`gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Open Sans`,
+            variants: [`300`,`400`,`500`,`600`,`700`,`800`]
+          },
+        ],
+      },
+    }
+  ],
 }
