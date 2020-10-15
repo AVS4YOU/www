1e6cbf658d0f846b9e69482edde87ac0a3708f1d
Shudrik Nikolai <46368637+ShudrikNikolai@users.noreply.github.com>
2020-10-15 13:40:03 +0300

    fix products-info

--- Client/static/products-info.js
+++ Client/static/products-info.js
@@ -1,32 +1,32 @@
 let VideoEditor = {
-    version: "9.4.1.360",
-    releaseDD: "07",
-    releaseMM: "07",
+    version: "9.4.2.369",
+    releaseDD: "05",
+    releaseMM: "10",
     releaseYYYY: "2020",
-    size: "161.58",
+    size: "161.12",
     platforms: "Windows 10, 8.1, 8, 7, XP, 2003, Vista (no Mac OS/ Linux support)"
 };
 
 let VideoReMaker = {
-    version: "6.4.1.240",
-    releaseDD: "07",
-    releaseMM: "07",
+    version: "6.4.2.245",
+    releaseDD: "05",
+    releaseMM: "10",
     releaseYYYY: "2020",
-    size: "59.13",
+    size: "59.51",
     platforms: "Windows 10, 8.1, 8, 7, XP, 2003, Vista (no Mac OS/ Linux support)"
 };
 
 let VideoConverter = {
-    version: "12.1.1.660",
-    releaseDD: "07",
-    releaseMM: "07",
+    version: "12.1.2.669",
+    releaseDD: "05",
+    releaseMM: "10",
     releaseYYYY: "2020",
-    size: "81.11",
+    size: "81.10",
     platforms: "Windows 10, 8.1, 8, 7, XP, 2003, Vista (no Mac OS/ Linux support)"
 };
 
 let MediaPlayer = {
-    version: "5.2.1.138",
+    version: "5.2.2.140",
     releaseDD: "07",
     releaseMM: "07",
     releaseYYYY: "2020",
@@ -35,20 +35,20 @@ let MediaPlayer = {
 };
 
 let AudioEditor = {
-    version: "10.0.1.547",
-    releaseDD: "07",
-    releaseMM: "07",
+    version: "10.0.2.550",
+    releaseDD: "01",
+    releaseMM: "10",
     releaseYYYY: "2020",
-    size: "47.14",
+    size: "48.17",
     platforms: "Windows 10, 8.1, 8, 7, XP, 2003, Vista (no Mac OS/ Linux support)"
 };
 
 let AudioConverter = {
-    version: "10.0.1.607",
-    releaseDD: "07",
-    releaseMM: "07",
+    version: "10.0.2.610",
+    releaseDD: "29",
+    releaseMM: "09",
     releaseYYYY: "2020",
-    size: "49.62",
+    size: "50.00",
     platforms: "Windows 10, 8.1, 8, 7, XP, 2003, Vista (no Mac OS/ Linux support)"
 };
 
