158552c1c575a8de23e139dc7d46421cb60dc34a
Vyacheslav-Belov-OO <63102499+Vyacheslav-Belov-OO@users.noreply.github.com>
2020-10-27 12:02:48 +0300

    Update index.js

--- Client/src/components/link-href/index.js
+++ Client/src/components/link-href/index.js
@@ -10,7 +10,7 @@ function AvsLinkHref(props){
                     } 
                 >   
 
-                    {console.log(props.toLink === "=")}
+                    
                     {props.children}
                 </a>   
             )}              
