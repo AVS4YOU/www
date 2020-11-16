6371cd186bd5199a021d5a5ec1c4c82eefe5da3c
Nik <anonnik5@gmail.com>
2020-11-16 16:05:33 +0300

    Update layout.js

--- Client/src/components/layout.js
+++ Client/src/components/layout.js
@@ -130,6 +130,17 @@ class Layout extends React.PureComponent {
             `}
           </script>
 
+          <script>
+            {`
+            (function(w,d,t,r,u){var f,n,i;w[u]=w[u]||[],
+            f=function(){var o={ti:"4024645"};o.q=w[u],w[u]=new UET(o),w[u].push("pageLoad")}
+            ,n=d.createElement(t),n.src=r,n.async=1,n.onload=n.onreadystatechange=function()
+            {var s=this.readyState;s&&s!=="loaded"&&s!=="complete"||(f(),n.onload=n.onreadystatechange=null)}
+            ,i=d.getElementsByTagName(t)[0],i.parentNode.insertBefore(n,i)})
+            (window,document,"script","//bat.bing.com/bat.js","uetq");
+            `}
+          </script>
+
           <script src={withPrefix('impact-write-cookie.js')} type="text/javascript" />
         </Helmet>
         {!this.props.headerIsDisabled && <Header availableLocales={this.props.pageContext.availableLocales} locale={this.props.pageContext.locale} t={this.props.t}/>}
