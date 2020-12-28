e13724678a6288efe131729e76e98de13b62b908
Nik <anonnik5@gmail.com>
2020-12-28 15:50:52 +0300

    update

--- Client/src/components/link-href/index.js
+++ Client/src/components/link-href/index.js
@@ -6,7 +6,8 @@ function AvsLinkHref(props){
     return(
         <PageContext.Consumer>  
             {(pageContext) => (
-                <a className={props.className}   href={ (pageContext.locale === "en" || pageContext.locale === "ru") ?  (props.mainLink === "/" ? "" : props.mainLink) + (props.toLink === ".pdf" ? "" : (pageContext.locale === "ru" ? "" : "/")) + (props.toLink === "=" ? "" : props.toLink) : 
+                <a className={props.className}   href={ (pageContext.locale === "en" || pageContext.locale === "ru"
+                || pageContext.locale === "nl" || pageContext.locale === "kr" || pageContext.locale === "pl" || pageContext.locale === "dk") ?  (props.mainLink === "/" ? "" : props.mainLink) + (props.toLink === ".pdf" ? "" : (pageContext.locale === "ru" ? "" : "/")) + (props.toLink === "=" ? "" : props.toLink) : 
                 props.mainLink + (props.toLink !== ".pdf" ? "" : "_") + pageContext.locale + (props.toLink === ".pdf" ? "" : props.toLink === "=" ? "" : "/") + (props.toLink === "=" ? "" : props.toLink)
                     } 
                 >   
