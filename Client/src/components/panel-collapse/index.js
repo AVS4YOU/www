b56c0cd94bd7ac40c81209da005c907971a1d4ee
Nik <anonnik5@gmail.com>
2021-02-08 15:11:39 +0300

    update

--- Client/src/components/panel-collapse/index.js
+++ Client/src/components/panel-collapse/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled, {css} from 'styled-components';
+import styled from 'styled-components';
 import PropTypes from "prop-types";
 import Text from "../text";
 import CloseIcon from '../../images/common/icons/close-popup.svg';
