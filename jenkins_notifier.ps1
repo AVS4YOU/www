3aac274cd0f489b0e88656ed81c79a38e19a86b7
YaroslavGorohov <yarikgorohov@gmail.com>
2021-02-17 15:40:22 +0300

    Update jenkins_notifier.ps1

--- jenkins_notifier.ps1
+++ jenkins_notifier.ps1
@@ -1,4 +1,4 @@
-$MESSAGE = "new.avs4you.com is deployed to amazon."
+$MESSAGE = "teststatic.avs4you.com is deployed to amazon."
 $TOKEN = "1341043651:AAGjwG5Wv4eo075KK_uvhZtw1fzWGH8NlU8"
 $CHAT_ID = "-1001346473906"
 $URL = "https://api.telegram.org/bot$TOKEN/sendMessage"
