18cc46350cbdc515e60b2bab2ab585007f58020b
Nik <anonnik5@gmail.com>
2021-03-19 12:41:48 +0300

    Update layout.js

--- Client/src/components/layout.js
+++ Client/src/components/layout.js
@@ -154,7 +154,7 @@ class Layout extends React.PureComponent {
           {this.props.metaKeywords && <meta name="keywords" content={this.props.metaKeywords} />}
           {this.props.pageContext.originalPath}
 
-          <link rel="canonical" href={"https://www.avs4you.com" + (this.props.pageContext.locale === "en" ? this.props.to : "/" + (this.props.pageContext.locale + "/")) + this.pageName}></link>
+          <link rel="canonical" href={"https://www.avs4you.com/" + (this.props.pageContext.locale === "en" ? this.props.to : "" + (this.props.pageContext.locale + "/")) + this.pageName}></link>
           <script src="https://secure.avangate.com/content/check_affiliate_v2.js"></script>
           {languageCodes.map((languageCode)=> {
             let language = languageCode.split("-")[0];
