bcbf4058f708d8bdddac1a7ae6f939f7e452c27b
Nik <anonnik5@gmail.com>
2021-03-29 12:28:24 +0300

    pages: fix src param

--- Client/src/pages/installed-audio-editor.aspx.js
+++ Client/src/pages/installed-audio-editor.aspx.js
@@ -39,7 +39,7 @@ class installedAudioEditor extends React.PureComponent {
     };
 
     this.state = {
-        hrefUnlim: this.cookies.get("Site_Tracing") ? shareItHrefUnlim + `&languageid=1&currency=USD&affiliate=${this.affiliateID}` : this.props.t("defaultHrefUnlim"),
+        hrefUnlim: this.cookies.get("Site_Tracing") ? shareItHrefUnlim + `&languageid=1&currency=USD&affiliate=${this.affiliateID}` : this.props.t("defaultHrefUnlim") + `&SRC=ThanksInstallation_AE_${this.props.t("en")}`,
         documentLoaded: false,
       };
   }
--- Client/src/pages/installed-video-editor.aspx.js
+++ Client/src/pages/installed-video-editor.aspx.js
@@ -39,7 +39,7 @@ class installedVideoEditor extends React.PureComponent {
     };
 
     this.state = {
-        hrefUnlim: this.cookies.get("Site_Tracing") ? shareItHrefUnlim + `&languageid=1&currency=USD&affiliate=${this.affiliateID}` : this.props.t("defaultHrefUnlim"),
+        hrefUnlim: this.cookies.get("Site_Tracing") ? shareItHrefUnlim + `&languageid=1&currency=USD&affiliate=${this.affiliateID}` : this.props.t("defaultHrefUnlim") + `&SRC=ThanksInstallation_VE_${this.props.t("en")}`,
         documentLoaded: false,
       };
   }
