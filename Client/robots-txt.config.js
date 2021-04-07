de3bcb4a517e2449000b4c1987fa90021a3db43e
Nik <anonnik5@gmail.com>
2021-04-07 12:41:03 +0300

    Update robots-txt.config.js

--- Client/robots-txt.config.js
+++ Client/robots-txt.config.js
@@ -1,36 +1,30 @@
-const userAgentID = ['Googlebot', 'Yandex', '*', '/*?'];
-
-const policeDisallow = [
-             '/404',
-             'da/404',
-             'de/404',
-             'es/404',
-             'fr/404',
-             'it/404',
-             'jp/404',
-             'ko/404',
-             'nl/404',
-             'pl/404',
-             'pt/404',
-             'ru/404',
-             '/advent-calendar.aspx',
-             'da/advent-calendar.aspx',
-             'de/advent-calendar.aspx',
-             'es/advent-calendar.aspx',
-             'fr/advent-calendar.aspx',
-             'it/advent-calendar.aspx',
-             'jp/advent-calendar.aspx',
-             'ko/advent-calendar.aspx',
-             'nl/advent-calendar.aspx',
-             'pl/advent-calendar.aspx',
-             'pt/advent-calendar.aspx',
-             'ru/advent-calendar.aspx',
+const userAgentID = ['Googlebot', 'Yandex', '*'];
+const langID = ['en', 'da', 'de', 'es', 'fr', 'it', 'jp', 'ko', 'nl', 'pl', 'pt', 'ru'];
+const disFolderId = ['/*?', '/static', '/page-data'];
+const pageID = [
+    '/advent-calendar.aspx',
+    '/mov-video-editor.aspx',
+    '/powerful-video-maker.aspx',
+    '/precise-video-cutting-tools.aspx',
 ];
 
+let policeDisallow = [];
+
+policeDisallow.push(...disFolderId);
+
+for(let i = 0; i < langID.length; i++){
+    for(let j = 0; j < pageID.length; j++){
+        if(i !== 0){
+            policeDisallow.push(langID[i] + pageID[j]);
+        } else{
+            policeDisallow.push(pageID[j]);
+        }
+    }
+}
 
 module.exports = {
     host: 'https://www.avs4you.com',
     sitemap: 'https://www.avs4you.com/sitemap.xml',
-    policy: [{ userAgent: [...userAgentID], disallow: [...policeDisallow] }],
+    policy: [{ userAgent: userAgentID[2], disallow: [...policeDisallow] }],
 };
 
