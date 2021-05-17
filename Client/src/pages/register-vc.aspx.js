97be7d431939eddc8d8a098af3607edaf75211c6
Nik <anonnik5@gmail.com>
2021-05-17 13:02:25 +0300

    modal-reg: fix image

--- Client/src/pages/register-vc.aspx.js
+++ Client/src/pages/register-vc.aspx.js
@@ -22,6 +22,7 @@ import PeopleIcon from '../images/register/register-people-icon.svg';
 
 import ModalLogoBtn from '../images/moneyback/logo.svg';
 import ModalCloseBtn from '../images/moneyback/close.svg';
+import ModalIndicatorCheck from '../images/moneyback/indicator-check.svg';
 
 
 const shareItHrefUnlim = "https://order.shareit.com/cart/add?vendorid=200281390&PRODUCT[300919255]=1";
@@ -214,7 +215,7 @@ render(){
             color: #f9f9f9;
           }
           .IndicatorCheck{
-            background-image: url("indicator-check.svg");
+            background-image: url(${ModalIndicatorCheck});
             width: 17px;
             padding: 5px 15px 10px 10px;
             content: ' ';
