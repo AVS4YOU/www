3f8acef8b14a95dfe6ff1c326903aa90f4dd9f28
SvetlanaLisina <svetik_lisik@mail.ru>
2022-02-01 11:22:08 +0300

    fix version products

--- Client/static/products-info.js
+++ Client/static/products-info.js
@@ -1,53 +1,53 @@
 let VideoEditor = {
-    version: "9.6.1.390",
-    releaseDD: "23",
-    releaseMM: "12",
-    releaseYYYY: "2021",
+    version: "9.6.2.391",
+    releaseDD: "31",
+    releaseMM: "01",
+    releaseYYYY: "2022",
     size: "170.40",
     platforms: "Windows 11, 10, 8.1, 8, 7, Vista, XP (no Mac OS/ Linux support)"
 };
 
 let VideoReMaker = {
-    version: "6.6.1.258",
-    releaseDD: "22",
-    releaseMM: "12",
-    releaseYYYY: "2021",
-    size: "59.86",
+    version: "6.6.2.259",
+    releaseDD: "01",
+    releaseMM: "31",
+    releaseYYYY: "2022",
+    size: "59.85",
     platforms: "Windows 11, 10, 8.1, 8, 7, Vista, XP (no Mac OS/ Linux support)"
 };
 
 let VideoConverter = {
-    version: "12.3.1.689",
-    releaseDD: "22",
-    releaseMM: "12",
-    releaseYYYY: "2021",
-    size: "80.16",
+    version: "12.3.2.690",
+    releaseDD: "31",
+    releaseMM: "01",
+    releaseYYYY: "2022",
+    size: "80.15",
     platforms: "Windows 11, 10, 8.1, 8, 7, Vista, XP (no Mac OS/ Linux support)"
 };
 
 let MediaPlayer = {
-    version: "5.4.1.147",
-    releaseDD: "22",
-    releaseMM: "12",
-    releaseYYYY: "2021",
-    size: "104.65",
+    version: "5.4.2.148",
+    releaseDD: "31",
+    releaseMM: "01",
+    releaseYYYY: "2022",
+    size: "104.62",
     platforms: "Windows 11, 10, 8.1, 8, 7, Vista, XP (no Mac OS/ Linux support)"
 };
 
 let AudioEditor = {
-    version: "10.2.1.562",
-    releaseDD: "22",
-    releaseMM: "12",
-    releaseYYYY: "2021",
-    size: "47.35",
+    version: "10.2.2.563",
+    releaseDD: "31",
+    releaseMM: "01",
+    releaseYYYY: "2022",
+    size: "47.32",
     platforms: "Windows 11, 10, 8.1, 8, 7, Vista, XP (no Mac OS/ Linux support)"
 };
 
 let AudioConverter = {
-    version: "10.2.1.629",
-    releaseDD: "22",
-    releaseMM: "12",
-    releaseYYYY: "2021",
+    version: "10.2.2.631",
+    releaseDD: "31",
+    releaseMM: "01",
+    releaseYYYY: "2022",
     size: "49.29",
     platforms: "Windows 11, 10, 8.1, 8, 7, Vista, XP (no Mac OS/ Linux support)"
 };
