e220d692039f9b79be5c84d39b1a94f336826f1c
SvetlanaLisina <svetik_lisik@mail.ru>
2022-06-17 16:36:41 +0300

    updated title

--- Client/src/pages/video-editor.aspx.js
+++ Client/src/pages/video-editor.aspx.js
@@ -410,10 +410,8 @@ class avsVideoEditor extends React.PureComponent {
         getDevice={this.getDevice}
         pageContext={this.props.pageContext}
         t={this.props.t}
-        title={this.props.t("AVS Audio Editor record audio, cut, mix audio files, delete audio parts, edit mp3")}
-        metaDescription={this.props.t("Record audio cut, split, mix audio tracks, delete audio parts apply audio effects and filters save audio to MP3 WAV WMA M4A FLAC")}
-        metaKeywords={this.props.t("avs audio editor, avs music editor, avs audio  creator, audio editor, audio editing software, mp3 editor, music editor, download  music editor, audio editor download, audio edit software, edit mp3, movie maker edit audio, edit audio clips, edit audio windows 7, cool edit audio, record audio,  record voice, mix audio, mix audio software, mix music, cut audio cd, cut audio  clips, cut audio from video, create audiobook, create audio, create audio file,  create audio from video, edit music, edit audio, edit song, edit mp3 songs")}
-      >
+        title={this.props.t("AVS Video Editor - easy video editing software for Windows")}
+       >
         <StyledVideo>
           <ProductPagesWrapper>
             <div className="headerBackground">
