150a0405dd5b63d1a2e07151c21e9561fff1f790
Vyacheslav-Belov-OO <vyacheslav.belov@onlyoffice.com>
2022-08-09 14:59:33 +0300

    fix products versions

--- Client/static/products-info.js
+++ Client/static/products-info.js
@@ -1,36 +1,36 @@
 let VideoEditor = {
-    version: "9.7.1.396",
-    releaseDD: "26",
-    releaseMM: "05",
+    version: "9.7.2.397",
+    releaseDD: "28",
+    releaseMM: "07",
     releaseYYYY: "2022",
-    size: "170.55",
+    size: "170.58",
     platforms: "Windows 11, 10, 8.1, 8, 7, Vista, XP (no Mac OS/ Linux support)"
 };
 
 let VideoReMaker = {
-    version: "6.7.1.262",
-    releaseDD: "24",
-    releaseMM: "05",
+    version: "6.7.2.263",
+    releaseDD: "27",
+    releaseMM: "07",
     releaseYYYY: "2022",
-    size: "60.03",
+    size: "60.06",
     platforms: "Windows 11, 10, 8.1, 8, 7, Vista, XP (no Mac OS/ Linux support)"
 };
 
 let VideoConverter = {
-    version: "12.4.1.695",
-    releaseDD: "26",
-    releaseMM: "05",
+    version: "12.4.2.696",
+    releaseDD: "28",
+    releaseMM: "07",
     releaseYYYY: "2022",
-    size: "80.37",
+    size: "80.40",
     platforms: "Windows 11, 10, 8.1, 8, 7, Vista, XP (no Mac OS/ Linux support)"
 };
 
 let MediaPlayer = {
-    version: "5.5.1.150",
-    releaseDD: "26",
-    releaseMM: "05",
+    version: "5.5.2.151",
+    releaseDD: "28",
+    releaseMM: "07",
     releaseYYYY: "2022",
-    size: "104.88",
+    size: "104.94",
     platforms: "Windows 11, 10, 8.1, 8, 7, Vista, XP (no Mac OS/ Linux support)"
 };
 
