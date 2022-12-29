e1154e2635f7a902fe9b989b23738aabc15ea878
SvetlanaLisina <svetik_lisik@mail.ru>
2022-12-29 12:29:35 +0300

    review-slider: added swipe

--- Client/src/components/reviews-slider-new/index.js
+++ Client/src/components/reviews-slider-new/index.js
@@ -30,7 +30,7 @@ const StyledCarouselWrapper = styled.div`
   .slick-list{
     padding-bottom: 20px;
   }
-  
+
   @media (max-width: 750px) {
     .slick-list{
       padding-bottom: 10;
@@ -49,6 +49,12 @@ const StyledCarouselWrapper = styled.div`
       width: 100%;
     }
   }
+`;
+
+const StyledIframe = styled.iframe`
+    position: relative;
+    z-index: -1;
+    border: none;
 `;
 
 const ReviewSliderNew = props => {
@@ -97,15 +103,15 @@ const ReviewSliderNew = props => {
             isVideoEditor &&
         <Slider {...settings} style={{touchAction:"pan-y"}}>
         
-            <iframe className="sliderFrame" id='gdm-vp-snippet-quotations_a74bc47b-9070-4fcb-9b89-f2c8a41d8428' src='https://datainsights-cdn.dm.aws.gartner.com/vp/snippet/a74bc47b-9070-4fcb-9b89-f2c8a41d8428' frameborder='0' scrolling='no' height='330px' ></iframe>
+            <StyledIframe className="sliderFrame" id='gdm-vp-snippet-quotations_a74bc47b-9070-4fcb-9b89-f2c8a41d8428' src='https://datainsights-cdn.dm.aws.gartner.com/vp/snippet/a74bc47b-9070-4fcb-9b89-f2c8a41d8428' frameborder='0' scrolling='no' height='330px' ></StyledIframe>
             
-            <iframe id='gdm-vp-snippet-quotations_bd5c138a-3fcd-4121-ae0b-f7e0cd2a62fd' src='https://datainsights-cdn.dm.aws.gartner.com/vp/snippet/bd5c138a-3fcd-4121-ae0b-f7e0cd2a62fd' frameborder='0' scrolling='no' height='330px' ></iframe>
+            <StyledIframe id='gdm-vp-snippet-quotations_bd5c138a-3fcd-4121-ae0b-f7e0cd2a62fd' src='https://datainsights-cdn.dm.aws.gartner.com/vp/snippet/bd5c138a-3fcd-4121-ae0b-f7e0cd2a62fd' frameborder='0' scrolling='no' height='330px' ></StyledIframe>
             
-            <iframe id='gdm-vp-snippet-quotations_77097225-915a-4ed7-aa42-e3dd77cca1aa' src='https://datainsights-cdn.dm.aws.gartner.com/vp/snippet/77097225-915a-4ed7-aa42-e3dd77cca1aa' frameborder='0' scrolling='no' height='330px' ></iframe>
+            <StyledIframe id='gdm-vp-snippet-quotations_77097225-915a-4ed7-aa42-e3dd77cca1aa' src='https://datainsights-cdn.dm.aws.gartner.com/vp/snippet/77097225-915a-4ed7-aa42-e3dd77cca1aa' frameborder='0' scrolling='no' height='330px' ></StyledIframe>
             
-            <iframe id='gdm-vp-snippet-quotations_8401fde0-020b-45e9-889b-63e620d5d3a0' src='https://datainsights-cdn.dm.aws.gartner.com/vp/snippet/8401fde0-020b-45e9-889b-63e620d5d3a0' frameborder='0' scrolling='no' height='330px' ></iframe>
+            <StyledIframe id='gdm-vp-snippet-quotations_8401fde0-020b-45e9-889b-63e620d5d3a0' src='https://datainsights-cdn.dm.aws.gartner.com/vp/snippet/8401fde0-020b-45e9-889b-63e620d5d3a0' frameborder='0' scrolling='no' height='330px' ></StyledIframe>
             
-            <iframe id='gdm-vp-snippet-quotations_77097225-915a-4ed7-aa42-e3dd77cca1aa' src='https://datainsights-cdn.dm.aws.gartner.com/vp/snippet/77097225-915a-4ed7-aa42-e3dd77cca1aa' frameborder='0' scrolling='no' height='330px' ></iframe>
+            <StyledIframe id='gdm-vp-snippet-quotations_77097225-915a-4ed7-aa42-e3dd77cca1aa' src='https://datainsights-cdn.dm.aws.gartner.com/vp/snippet/77097225-915a-4ed7-aa42-e3dd77cca1aa' frameborder='0' scrolling='no' height='330px' ></StyledIframe>
             
         </Slider>
 }
@@ -113,21 +119,21 @@ const ReviewSliderNew = props => {
             !isVideoEditor &&
         <Slider {...settings} style={{touchAction:"pan-y"}}>
         
-        <iframe id='gdm-vp-snippet-quotations_af0d15a6-1f3b-4cb7-8d89-cc68f14e7289' src='https://datainsights-cdn.dm.aws.gartner.com/vp/snippet/af0d15a6-1f3b-4cb7-8d89-cc68f14e7289' frameborder='0' scrolling='no' height='330px' ></iframe>
+        <StyledIframe id='gdm-vp-snippet-quotations_af0d15a6-1f3b-4cb7-8d89-cc68f14e7289' src='https://datainsights-cdn.dm.aws.gartner.com/vp/snippet/af0d15a6-1f3b-4cb7-8d89-cc68f14e7289' frameborder='0' scrolling='no' height='330px' ></StyledIframe>
 
-        <iframe id='gdm-vp-snippet-quotations_a02edb07-b011-46c9-bd45-a46e37e73256' src='https://datainsights-cdn.dm.aws.gartner.com/vp/snippet/a02edb07-b011-46c9-bd45-a46e37e73256' frameborder='0' scrolling='no' height='330px' ></iframe>
+        <StyledIframe id='gdm-vp-snippet-quotations_a02edb07-b011-46c9-bd45-a46e37e73256' src='https://datainsights-cdn.dm.aws.gartner.com/vp/snippet/a02edb07-b011-46c9-bd45-a46e37e73256' frameborder='0' scrolling='no' height='330px' ></StyledIframe>
 
-        <iframe id='gdm-vp-snippet-quotations_bedfaa45-2263-484a-aa4e-96f37d4777cf' src='https://datainsights-cdn.dm.aws.gartner.com/vp/snippet/bedfaa45-2263-484a-aa4e-96f37d4777cf' frameborder='0' scrolling='no' height='330px' ></iframe>
+        <StyledIframe id='gdm-vp-snippet-quotations_bedfaa45-2263-484a-aa4e-96f37d4777cf' src='https://datainsights-cdn.dm.aws.gartner.com/vp/snippet/bedfaa45-2263-484a-aa4e-96f37d4777cf' frameborder='0' scrolling='no' height='330px' ></StyledIframe>
 
-        <iframe id='gdm-vp-snippet-quotations_fffdd00f-7d41-477e-bb35-830176fa1fc9' src='https://datainsights-cdn.dm.aws.gartner.com/vp/snippet/fffdd00f-7d41-477e-bb35-830176fa1fc9' frameborder='0' scrolling='no' height='330px' ></iframe>
+        <StyledIframe id='gdm-vp-snippet-quotations_fffdd00f-7d41-477e-bb35-830176fa1fc9' src='https://datainsights-cdn.dm.aws.gartner.com/vp/snippet/fffdd00f-7d41-477e-bb35-830176fa1fc9' frameborder='0' scrolling='no' height='330px' ></StyledIframe>
 
-        <iframe id='gdm-vp-snippet-quotations_70946988-3d6d-4dac-9eac-d28b20289c19' src='https://datainsights-cdn.dm.aws.gartner.com/vp/snippet/70946988-3d6d-4dac-9eac-d28b20289c19' frameborder='0' scrolling='no' height='330px' ></iframe>
+        <StyledIframe id='gdm-vp-snippet-quotations_70946988-3d6d-4dac-9eac-d28b20289c19' src='https://datainsights-cdn.dm.aws.gartner.com/vp/snippet/70946988-3d6d-4dac-9eac-d28b20289c19' frameborder='0' scrolling='no' height='330px' ></StyledIframe>
 
-        <iframe id='gdm-vp-snippet-quotations_484f1985-346a-4f2f-bba5-978c499b3ba7' src='https://datainsights-cdn.dm.aws.gartner.com/vp/snippet/484f1985-346a-4f2f-bba5-978c499b3ba7' frameborder='0' scrolling='no' height='330px' ></iframe>
+        <StyledIframe id='gdm-vp-snippet-quotations_484f1985-346a-4f2f-bba5-978c499b3ba7' src='https://datainsights-cdn.dm.aws.gartner.com/vp/snippet/484f1985-346a-4f2f-bba5-978c499b3ba7' frameborder='0' scrolling='no' height='330px' ></StyledIframe>
 
-        <iframe id='gdm-vp-snippet-quotations_9275f2ae-2be5-473e-8d94-702aa5ed5804' src='https://datainsights-cdn.dm.aws.gartner.com/vp/snippet/9275f2ae-2be5-473e-8d94-702aa5ed5804' frameborder='0' scrolling='no' height='330px' ></iframe>
+        <StyledIframe id='gdm-vp-snippet-quotations_9275f2ae-2be5-473e-8d94-702aa5ed5804' src='https://datainsights-cdn.dm.aws.gartner.com/vp/snippet/9275f2ae-2be5-473e-8d94-702aa5ed5804' frameborder='0' scrolling='no' height='330px' ></StyledIframe>
 
-        <iframe id='gdm-vp-snippet-quotations_bd24f0b6-3d8a-40fc-89b9-1660bc4124f0' src='https://datainsights-cdn.dm.aws.gartner.com/vp/snippet/bd24f0b6-3d8a-40fc-89b9-1660bc4124f0' frameborder='0' scrolling='no' height='330px' ></iframe>
+        <StyledIframe id='gdm-vp-snippet-quotations_bd24f0b6-3d8a-40fc-89b9-1660bc4124f0' src='https://datainsights-cdn.dm.aws.gartner.com/vp/snippet/bd24f0b6-3d8a-40fc-89b9-1660bc4124f0' frameborder='0' scrolling='no' height='330px' ></StyledIframe>
         </Slider>
 }
       </StyledCarouselWrapper>
