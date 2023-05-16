3cd5ccc03cce20fecff3d914a60982fac8ab62f7
Svetlana Lisina <svetik_lisik@mail.ru>
2023-05-16 10:44:37 +0300

    Updated deploy.sh

--- .jenkins/deploy.sh
+++ .jenkins/deploy.sh
@@ -135,9 +135,7 @@ check_deploy() {
   echo "=== check deploy ==="
 
   
-  curl -Is http://"${name_new_bucket}".s3-website-us-east-1.amazonaws.com \ 
-    | head -n 1 \
-    | grep 200
+  curl -Is http://"${name_new_bucket}".s3-website-us-east-1.amazonaws.com | head -n 1 | grep 200
     
   if [[ $? -eq 0 ]]; then
     echo "site OK"
