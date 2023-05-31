187c4b6136630bb2e8d7d5e1d1bf6e2224c78538
Svetlana Lisina <svetik_lisik@mail.ru>
2023-05-31 17:30:38 +0300

    added affiliate checker code

--- Client/src/pages/register.aspx.js
+++ Client/src/pages/register.aspx.js
@@ -100,7 +100,28 @@ render(){
        metaKeywords=""
       >
         <Helmet>
+          
         {<script type='text/javascript'>{`
+    (function (document, src, libName, config) {
+      var script             = document.createElement('script';);
+      script.src             = src;
+      script.async           = true;
+      var firstScriptElement = document.getElementsByTagName('script')[0];
+      script.onload          = function () {
+          for (var namespace in config) {
+              if (config.hasOwnProperty(namespace)) {
+                  window[libName].setup.setConfig(namespace, config[namespace]);
+              }
+          }
+          window[libName].register();
+      };
+
+      firstScriptElement.parentNode.insertBefore(script, firstScriptElement);
+  })(document, 'https://secure.2checkout.com/checkout/client/twoCoInlineCart.js';, 'TwoCoInlineCart';,{"app":{"merchant":"ONLMETEC","iframeLoad":"checkout"},"cart":{"host":"https:\/\/secure.2checkout.com","customization":"ONLMETEC-inline-one-step"}});
+              `}
+          </script>}
+
+          {<script type='text/javascript'>{`
                   window._vwo_code = window._vwo_code || (function(){
                   var account_id=279977,
                   settings_tolerance=5000,
