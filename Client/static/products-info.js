15624bff627e7d24e79cc5b339130efba171eeb7
Svetlana Lisina <svetik_lisik@mail.ru>
2023-07-28 16:20:31 +0300

    fixed product info

--- Client/static/products-info.js
+++ Client/static/products-info.js
@@ -1,52 +1,52 @@
 let VideoEditor = {
-    version: "9.9.1.407",
-    releaseDD: "04",
-    releaseMM: "05",
+    version: "9.9.2.408",
+    releaseDD: "27",
+    releaseMM: "07",
     releaseYYYY: "2023",
     size: "170.62",
     platforms: "Windows 11, 10, 8.1, 8, 7, Vista, XP (no Mac OS/ Linux support)"
 };
 
 let VideoReMaker = {
-    version: "6.8.1.268",
-    releaseDD: "04",
-    releaseMM: "05",
+    version: "6.8.2.269",
+    releaseDD: "27",
+    releaseMM: "07",
     releaseYYYY: "2023",
-    size: "60.08",
+    size: "60.09",
     platforms: "Windows 11, 10, 8.1, 8, 7, Vista, XP (no Mac OS/ Linux support)"
 };
 
 let VideoConverter = {
-    version: "12.6.1.700",
-    releaseDD: "04",
-    releaseMM: "05",
+    version: "12.6.2.701",
+    releaseDD: "27",
+    releaseMM: "07",
     releaseYYYY: "2023",
     size: "80.43",
     platforms: "Windows 11, 10, 8.1, 8, 7, Vista, XP (no Mac OS/ Linux support)"
 };
 
 let MediaPlayer = {
-    version: "5.6.1.154",
-    releaseDD: "04",
-    releaseMM: "05",
+    version: "5.6.2.155",
+    releaseDD: "27",
+    releaseMM: "07",
     releaseYYYY: "2023",
-    size: "105",
+    size: "105.01",
     platforms: "Windows 11, 10, 8.1, 8, 7, Vista, XP (no Mac OS/ Linux support)"
 };
 
 let AudioEditor = {
-    version: "10.4.1.570",
-    releaseDD: "04",
-    releaseMM: "05",
+    version: "10.4.2.571",
+    releaseDD: "27",
+    releaseMM: "07",
     releaseYYYY: "2023",
-    size: "47.58",
+    size: "47.56",
     platforms: "Windows 11, 10, 8.1, 8, 7, Vista, XP (no Mac OS/ Linux support)"
 };
 
 let AudioConverter = {
-    version: "10.4.1.636",
-    releaseDD: "04",
-    releaseMM: "05",
+    version: "10.4.2.637",
+    releaseDD: "27",
+    releaseMM: "07",
     releaseYYYY: "2023",
     size: "49.56",
     platforms: "Windows 11, 10, 8.1, 8, 7, Vista, XP (no Mac OS/ Linux support)"
