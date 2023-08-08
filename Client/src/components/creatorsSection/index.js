aa4dc6621cb620713a494a37ec5736c7343ec52c
Svetlana Lisina <svetik_lisik@mail.ru>
2023-08-08 18:25:17 +0300

    bugfix

--- Client/src/components/creatorsSection/index.js
+++ Client/src/components/creatorsSection/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import SectionTitle from "../sectionTitle";
+import {SectionTitle} from "../sectionTitle";
 import {CreatorsSectionCarousel} from "../creatorsSectionCarousel";
 
 const CreatorsSectionStyled = styled.section`
--- Client/src/components/makeVideoSection/index.js
+++ Client/src/components/makeVideoSection/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import SectionTitle from "../sectionTitle";
+import {SectionTitle} from "../sectionTitle";
 import {MakeVideSectionCarousel} from "../makeVideSectionCarousel";
 
 const MakeVideoSectionStyled = styled.section`
