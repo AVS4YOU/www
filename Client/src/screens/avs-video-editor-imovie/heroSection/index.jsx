f89c4d5ff95b7515571622ac0260acca514ea5d6
khikmatjon <khikmatjon@medio.uz>
2023-12-14 12:36:08 +0500

    changed bg video to bg image

--- Client/src/screens/avs-video-editor-imovie/heroSection/index.jsx
+++ Client/src/screens/avs-video-editor-imovie/heroSection/index.jsx
@@ -4,7 +4,7 @@ import {
     HeroSectionStyled,
     HeroSectionContainer,
     HeroSectionTitle,
-    HeroSectionDesc, HeroSectionLink, HeroSectionBackgroundVideo,
+    HeroSectionDesc, HeroSectionLink,
 } from './heroSection.styled'
 
 export const HeroSection = ({t}) => {
@@ -23,7 +23,6 @@ export const HeroSection = ({t}) => {
                     <span>{t('Windows 11/10/8/7/Vista/XP')}</span>
                 </HeroSectionLink>
             </HeroSectionContainer>
-            <HeroSectionBackgroundVideo autoPlay loop muted src="https://d2nlctn12v279m.cloudfront.net/media/newsletters/november2023/production_id4990248.mp4" />
         </HeroSectionStyled>
     )
 }
