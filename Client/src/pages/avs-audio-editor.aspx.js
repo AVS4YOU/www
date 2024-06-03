3b3d6daf5917ae37693b1ccf1ba71672e21f4fe5
Svetlana Lisina <svetik_lisik@mail.ru>
2024-06-03 12:32:07 +0300

    updated versions for products

--- Client/src/pages/avs-audio-editor.aspx.js
+++ Client/src/pages/avs-audio-editor.aspx.js
@@ -217,7 +217,7 @@ class avsAudioEditor extends React.PureComponent {
                                                                 style={{filter: "brightness(5)"}}/> Trustpilot
                         </Button>
                     </div>
-                    <Text id="screenshotsCarousel" className="carouselHeader" as="h2">AVS Audio Editor 10.4</Text>
+                    <Text id="screenshotsCarousel" className="carouselHeader" as="h2">AVS Audio Editor 10.5</Text>
                     <ScreenshotsCarousel
                         imageNames={[this.props.t("audio editor slider1 jpg"), this.props.t("audio editor slider2 jpg"), this.props.t("audio editor slider3 jpg"), this.props.t("audio editor slider4 jpg"), this.props.t("audio editor slider5 jpg"), this.props.t("audio editor slider6 jpg")]}
                         imageNamesPopup={[this.props.t("audio editor slider1 jpg"), this.props.t("audio editor slider2 jpg"), this.props.t("audio editor slider3 jpg"), this.props.t("audio editor slider4 jpg"), this.props.t("audio editor slider5 jpg"), this.props.t("audio editor slider6 jpg")]}
--- Client/static/products-info.js
+++ Client/static/products-info.js
@@ -1,54 +1,54 @@
 let VideoEditor = {
-    version: "9.9.4.412",
-    releaseDD: "01",
-    releaseMM: "02",
+    version: "10.0.1.421",
+    releaseDD: "30",
+    releaseMM: "05",
     releaseYYYY: "2024",
-    size: "172.91",
+    size: "172.26",
     platforms: "Windows 11, 10, 8.1, 8, 7, Vista, XP (no Mac OS/ Linux support)"
 };
 
 let VideoReMaker = {
-    version: "6.8.4.274",
-    releaseDD: "01",
-    releaseMM: "02",
+    version: "7.0.1.282",
+    releaseDD: "30",
+    releaseMM: "05",
     releaseYYYY: "2024",
-    size: "61.87",
+    size: "61.82",
     platforms: "Windows 11, 10, 8.1, 8, 7, Vista, XP (no Mac OS/ Linux support)"
 };
 
 let VideoConverter = {
-    version: "13.0.2.719",
-    releaseDD: "01",
-    releaseMM: "02",
+    version: "13.0.3.722",
+    releaseDD: "30",
+    releaseMM: "05",
     releaseYYYY: "2024",
-    size: "82.73",
+    size: "81.52",
     platforms: "Windows 11, 10, 8.1, 8, 7, Vista, XP (no Mac OS/ Linux support)"
 };
 
 let MediaPlayer = {
-    version: "5.6.4.158",
-    releaseDD: "01",
-    releaseMM: "02",
+    version: "5.7.1.159",
+    releaseDD: "30",
+    releaseMM: "05",
     releaseYYYY: "2024",
-    size: "108.29",
+    size: "106.57",
     platforms: "Windows 11, 10, 8.1, 8, 7, Vista, XP (no Mac OS/ Linux support)"
 };
 
 let AudioEditor = {
-    version: "10.4.4.575",
-    releaseDD: "01",
-    releaseMM: "02",
+    version: "10.5.1.577",
+    releaseDD: "30",
+    releaseMM: "05",
     releaseYYYY: "2024",
-    size: "48.70",
+    size: "48.21",
     platforms: "Windows 11, 10, 8.1, 8, 7, Vista, XP (no Mac OS/ Linux support)"
 };
 
 let AudioConverter = {
-    version: "10.4.4.641",
-    releaseDD: "01",
-    releaseMM: "02",
+    version: "10.5.1.642",
+    releaseDD: "30",
+    releaseMM: "05",
     releaseYYYY: "2024",
-    size: "50.92",
+    size: "50.20",
     platforms: "Windows 11, 10, 8.1, 8, 7, Vista, XP (no Mac OS/ Linux support)"
 };
 
