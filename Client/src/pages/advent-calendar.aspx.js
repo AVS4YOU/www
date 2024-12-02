8b8a3301a0f79b0dc8300337f6c1ba70926236bd
Svetlana Lisina <svetik_lisik@mail.ru>
2024-12-02 19:07:42 +0300

    /advent-calendar.aspx: fixed link

--- Client/src/pages/advent-calendar.aspx.js
+++ Client/src/pages/advent-calendar.aspx.js
@@ -28,9 +28,9 @@ import iconFacebook from "../images/advent-calendar/facebook.svg";
 
 import { withSoundCloudAudio } from 'react-soundplayer/addons';
 
-const PostcardLink = '/static/avs_card_2025.zip';
-const PostcardLinkLetter = '/static/avs_letter_2025.zip';
-const PostcardLinkWallpaper = '/static/avs_wallpaper_2025.zip';
+const PostcardLink = '/avs_card_2025.zip';
+const PostcardLinkLetter = '/avs_letter_2025.zip';
+const PostcardLinkWallpaper = '/avs_wallpaper_2025.zip';
 
 const MenuWrstyle = styled.div`
 
